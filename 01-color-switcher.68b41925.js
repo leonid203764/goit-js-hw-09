2e283f41c1eaa8d5249b546242b8306fa3e79371
leonid203764 <108736813+leonid203764@users.noreply.github.com>
2022-12-25 18:25:27 +0000

    Deploying to gh-pages from @ leonid203764/goit-js-hw-09@885179673e27f57ec4d07af453d3bdecd8f281ab 🚀

---
+++ 01-color-switcher.68b41925.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,o=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.68b41925.js.map
